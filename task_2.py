7bc3e8a9dd821c205668426b48a939c9a6cf176e
Aleksandr Kostitsyn <kostitsin.a@mail.ru>
2020-01-25 21:53:31 +0400

    Почистил ветку master

--- task_2.py
+++
@@ -1,4 +0,0 @@
-"""Использовал новую фичу с f-строками, введеную с Python 3.8 {param=}"""
-
-print((lambda name, surname, year, city, email, phone: f'{name=}, {surname=}, {year=}, {city=}, {email=}, {phone=}')
-      ('Vasylyi', 'Pupkin', '1917', 'New York', 'pupkin.vas@mail.ru', '890001010'))
