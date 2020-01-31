f47da0bb2f7c0719209080c1f48187af0e295854
Aleksandr <kostitsin.a@mail.ru>
2020-01-31 16:13:58 +0400

    Ошибка

--- task_1.py
+++
@@ -1,5 +0,0 @@
-my_string = True
-while my_string:
-    my_string = input('Введите строку: ')
-    with open('text_1.txt', 'a') as f:
-        f.write(f'{my_string}\n')
--- task_3.py
+++
@@ -1,9 +0,0 @@
-with open('text_3.txt', 'r') as f:
-    sum = 0
-    print('Сотрудники с окладом менее 20000:')
-    for num, line in enumerate(f, 1):
-        a = line.split()
-        if float(a[1]) < 20000:
-            print(a[0])
-        sum += float(a[1])
-    print(f'Средняя зарплата сотрудников составляет {round(sum / num, 2)}')
